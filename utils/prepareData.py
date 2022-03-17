492c12d5eec94accf2cb7780e80929f1211eab10
zhangshqii <35323338+zhangshqii@users.noreply.github.com>
2022-03-17 18:42:57 +0800

    Add files via upload

---
+++ utils/prepareData.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+# @Project: STCGAT
+# @Author  : shiqiZhang
+# @Time    : 2022/2/26 12:52
+import csv
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+import scipy.sparse as sp
+
+
+def get_adjacent_matrix(distance_file: str, num_nodes: int, id_file: str = None, graph_type="distance")->np.array:
+
+    """
+    :param distance_file: str, path of csv file to save the distances between nodes.
+    :param num_nodes: int, number of nodes in the graph
+    :param id_file: str, path of txt file to save the order of the nodes.
+    :param graph_type: str, ["connect", "distance"]
+    :return:
+        np.array(N, N)
+    """
+    A = np.zeros([int(num_nodes), int(num_nodes)])
+
+    if id_file: # 把绝对编号转化为相对编号
+        with open(id_file, "r") as f_id:
+            node_id_dict = {int(node_id): idx for idx, node_id in enumerate(f_id.read().strip().split("\n"))}
+
+            with open(distance_file, "r") as f_d:
+                f_d.readline()
+                reader = csv.reader(f_d)
+                for item in reader:
+                    if len(item) != 3:
+                        continue
+                    i, j, distance = int(item[0]), int(item[1]), float(item[2])
+                    if graph_type == "connect":
+                        A[node_id_dict[i], node_id_dict[j]] = 1.
+                        A[node_id_dict[j], node_id_dict[i]] = 1.
+                    elif graph_type == "distance":
+                        A[node_id_dict[i], node_id_dict[j]] = 1. / distance
+                        A[node_id_dict[j], node_id_dict[i]] = 1. / distance
+                    else:
+                        raise ValueError("graph type is not correct (connect or distance)")
+        return A
+
+    with open(distance_file, "r") as f_d:
+        f_d.readline()  # 跳过第一行
+        reader = csv.reader(f_d)
+        for item in reader:
+            if len(item) != 3:
+                continue
+            i, j, distance = int(item[0]), int(item[1]), float(item[2])
+
+            if graph_type == "connect":
+                A[i, j], A[j, i] = 1., 1.
+            elif graph_type == "distance":
+                A[i, j] = 1. / distance
+                A[j, i] = 1. / distance
+            else:
+                raise ValueError("graph type is not correct (connect or distance)")
+
+    return A
+
+
+def preprocess_grap_A(adj):
+    # _A = A + I
+    _adj = adj + sp.eye(adj.shape[0])
+    # _dsqp
+    _dsep = _adj.sum(1).A1
+
+    _D_half = sp.diags(np.power(_dsep, -0.5))
+
+    adj_normalized = _D_half @ _adj @ _D_half
+    return adj_normalized
+
+
+def preprocess_grap_B(adj):
+    N = adj.size(0)
+    matrix_i = torch.eye(N, dtype=torch.float)
+    adj += matrix_i  # [N, N]   A+I
+
+    degree_matrix = torch.sum(adj, dim=1, keepdim=False)  # [N],
+    degree_matrix = degree_matrix.pow(-1)
+    degree_matrix[degree_matrix == float("inf")] = 0.
+
+    degree_matrix = torch.diag(degree_matrix)  # Convert to diagonal matrix
+
+    return torch.mm(degree_matrix, adj)  # \hat A=D^(-1) * A ,This is equivalent to \hat A = D_{-1/2}*A*D_{-1/2}
+
+
+def get_flow_data(flow_file: str) -> np.array:
+    """
+    :param flow_file: str, path of .npz file to save the traffic flow data
+    :return:
+        np.array(N, T, D)
+    """
+    data = np.load(flow_file)
+
+    flow_data = data['data'].transpose([1, 0, 2])[:, :, 0][:, :, np.newaxis]
+
+    return flow_data
+
+
+class LoadData(Dataset):
+    def __init__(self, data_path, Split_rate, history_length, predict_length, train_mode):
+        """
+        :param data_path: list, ["graph file name" , "flow data file name"], path to save the data file names.
+        :param num_nodes: int, number of nodes.
+        :param divide_days: list, [ days of train data, days of test data], list to divide the original data.
+        :param time_interval: int, time interval between two traffic data records (mins).
+        :param history_length: int, length of history data to be used.
+        :param train_mode: list, ["train", "test"].
+        """
+
+        self.data_path = data_path
+        self.train_mode = train_mode
+        self.train_rate = Split_rate[0]
+        self.test_rate = Split_rate[1]
+        self.history_length = history_length
+        self.predict_length = predict_length
+        self.flow_norm, self.flow_data = self.pre_process_data(data=get_flow_data(data_path), norm_dim=1)
+
+    def __len__(self):
+        """
+        :return: length of dataset (number of samples).
+        """
+        if self.train_mode == "train":
+            return int(self.flow_data.shape[1] * self.train_rate) - self.history_length - self.predict_length + 1
+        elif self.train_mode == "val":
+            return int(self.flow_data.shape[1] * self.test_rate) - self.history_length - self.predict_length + 1
+        elif self.train_mode == "test":
+            return int(self.flow_data.shape[1]) - int(self.flow_data.shape[1] * (self.train_rate + self.test_rate)) - self.history_length - self.predict_length + 1
+        else:
+            raise ValueError("train mode: [{}] is not defined".format(self.train_mode))
+
+    def __getitem__(self, index):  # (x, y), index = [0, L1 - 1]
+        """
+        :param index: int, range between [0, length - 1].
+        :return:
+            graph: torch.tensor, [N, N].
+            data_x: torch.tensor, [N, H, D].
+            data_y: torch.tensor, [N, 1, D].
+        """
+        if self.train_mode == "train":
+            index = index
+        elif self.train_mode == "val":
+            index += int(self.flow_data.shape[1] * self.train_rate)
+        elif self.train_mode == "test":
+            index += (int(self.flow_data.shape[1] * (self.train_rate + self.test_rate)))
+        else:
+            raise ValueError("train mode: [{}] is not defined".format(self.train_mode))
+
+        data_x, data_y = LoadData.slice_data(self.flow_data, self.history_length, self.predict_length, index)
+
+        data_x = LoadData.to_tensor(data_x)  # [N, H, D]
+        data_y = LoadData.to_tensor(data_y)  # [N, 1]
+
+        # 处理邻接矩阵 graph
+        # graph = preprocess_grap_A(get_adjacent_matrix(distance_file=self.data_path[0], num_nodes=data_x.shape[0]))
+        # return {"graph": LoadData.to_tensor(graph), "flow_x": data_x, "flow_y": data_y}
+
+        # graph = preprocess_grap_B(LoadData.to_tensor(
+        #     get_adjacent_matrix(distance_file=self.data_path[0], num_nodes=data_x.shape[0])))
+        # return {"graph": graph, "flow_x": data_x, "flow_y": data_y}
+
+        return {"flow_x": data_x, "flow_y": data_y}
+
+    @staticmethod
+    def slice_data(data, history_length, predict_length, index):
+        """
+        :param data: np.array, normalized traffic data.
+        :param history_length: int, length of history data to be used.
+        :param index: int, index on temporal axis.
+        :param train_mode: str, ["train", "test"].
+        :return:
+            data_x: np.array, [N, H, D].
+            data_y: np.array [N, D].
+        """
+
+        # start_index = index
+        # end_index = index + history_length
+        # print("+++++++++++++++++++")
+        # print("start_index:", start_index)
+        # print("end_index:", end_index)
+        # print("+++++++++++++++++++")
+        #
+        # data_x = data[:, start_index: end_index]
+        # data_y = data[:, end_index: predict_length]
+        #
+        # print("*******************")
+        # print("end_index:", end_index)
+        # print("end_index+predict_length:", end_index+predict_length)
+        # print("*******************")
+
+        start_index = index
+        end_index = index + history_length + predict_length
+
+        data_w = data[:, start_index: end_index]
+        data_x, data_y = np.split(data_w, 2, axis=1)
+
+        return data_x, data_y
+
+    @staticmethod
+    def pre_process_data(data, norm_dim):
+        """
+        :param data: np.array, original traffic data without normalization.
+        :param norm_dim: int, normalization dimension.
+        :return:
+            norm_base: list, [max_data, min_data], data of normalization base.
+            norm_data: np.array, normalized traffic data.
+        """
+        norm_base = LoadData.normalize_base(data, norm_dim)  # find the normalize base
+        norm_data = LoadData.normalize_data(norm_base[0], norm_base[1], data)  # normalize data
+
+        return norm_base, norm_data
+
+    @staticmethod
+    def normalize_base(data, norm_dim):
+        """
+        :param data: np.array, original traffic data without normalization.
+        :param norm_dim: int, normalization dimension.
+        :return:
+            max_data: np.array
+            min_data: np.array
+        """
+        max_data = np.max(data, norm_dim, keepdims=True)  # [N, T, D] , norm_dim=1, [N, 1, D]
+        min_data = np.min(data, norm_dim, keepdims=True)
+        return max_data, min_data
+
+    @staticmethod
+    def normalize_data(max_data, min_data, data):
+        """
+        :param max_data: np.array, max data.
+        :param min_data: np.array, min data.
+        :param data: np.array, original traffic data without normalization.
+        :return:
+            np.array, normalized traffic data.
+        """
+        mid = min_data
+        base = max_data - min_data
+        normalized_data = (data - mid) / base
+
+        return normalized_data
+
+    @staticmethod
+    def recover_data(max_data, min_data, data):
+        """
+        :param max_data: np.array, max data.
+        :param min_data: np.array, min data.
+        :param data: np.array, normalized data.
+        :return:
+            recovered_data: np.array, recovered data.
+        """
+        mid = min_data
+        base = max_data - min_data
+
+        recovered_data = data * base + mid
+
+        return recovered_data
+
+    @staticmethod
+    def to_tensor(data):
+        return torch.tensor(data, dtype=torch.float)
+
+
+if __name__ == '__main__':
+    train_data = LoadData(data_path=["../datasets/PEMS04/PEMS04.csv", "../datasets/PEMS04/PEMS04.npz"], Split_rate=[0.6, 0.2],
+                          history_length=12, predict_length=12, train_mode="train")
+
+    val_data = LoadData(data_path=["../datasets/PEMS04/PEMS04.csv", "../datasets/PEMS04/PEMS04.npz"], Split_rate=[0.6, 0.2],
+                        history_length=12, predict_length=12, train_mode="val")
+
+    test_data = LoadData(data_path=["../datasets/PEMS04/PEMS04.csv", "../datasets/PEMS04/PEMS04.npz"], Split_rate=[0.6, 0.2],
+                         history_length=12, predict_length=12, train_mode="test")
+    print("------------------------------------------------------")
+    print(len(train_data))
+    print(train_data[0]["flow_x"].size())
+    print(train_data[0]["flow_y"].size())
+
+    print(len(val_data))
+    print(val_data[0]["flow_x"].size())
+    print(val_data[0]["flow_y"].size())
+
+    print(len(test_data))
+    print(test_data[0]["flow_x"].size())
+    print(test_data[0]["flow_y"].size())
---
+++ utils/utils.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# @Project: trafficPrediction
+# @Author  : shiqiZhang
+# @Time    : 2021/10/11 14:56
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import torch
+import xlwt
+from utils.metrics import masked_mae, masked_rmse_test, masked_mse, masked_mape_np, r2
+
+
+class Evaluation(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def mae_(target, output):
+        return np.mean(np.abs(target - output))
+
+    @staticmethod
+    def mape_(target, output):
+        return np.mean(np.abs(target - output) / (target + 5))
+
+    @staticmethod
+    def rmse_(target, output):
+        return np.sqrt(np.mean(np.power(target - output, 2)))
+
+    @staticmethod
+    def mse_(target, output):
+        return np.sum(((target - output)**2)/len(target))
+
+    @staticmethod
+    def smape(y_true, y_pred):
+        return 2.0 * np.mean(np.abs(y_pred - y_true) / (np.abs(y_pred) + np.abs(y_true))) * 100
+
+    @staticmethod
+    def total(target, output):
+        mae = Evaluation.mae_(target, output)
+        # mape = Evaluation.mape_(target, output)
+        smape = Evaluation.smape(target, output)
+        rmse = Evaluation.rmse_(target, output)
+
+        mse = Evaluation.mse_(target, output)
+        var = np.var(target)
+        R2 = 1 - mse/var
+
+        return mae, smape, rmse, R2
+
+    @staticmethod
+    def total_3(label, pred):
+        # mae = Evaluation.mask_mae_(pred, label, 0)
+        # rmse = np.square(mae)
+
+        with np.errstate(divide='ignore', invalid='ignore'):
+            mask = np.not_equal(label, 0)
+            mask = mask.astype(np.float32)
+            mask /= np.mean(mask)
+            mae = np.abs(np.subtract(pred, label)).astype(np.float32)
+            # smape = np.abs(label - pred) / (np.abs(pred) + np.abs(label))
+            rmse = np.square(mae)
+            mape = np.divide(mae, label)
+            mae = np.nan_to_num(mae * mask)
+
+            # smape = np.nan_to_num(smape * mask)
+
+            # wape = np.divide(np.sum(mae), np.sum(label))
+            mae = np.mean(mae)
+
+            # smape = 2.0 * np.mean(smape) * 100
+
+            rmse = np.nan_to_num(rmse * mask)
+            rmse = np.sqrt(np.mean(rmse))
+            mape = np.nan_to_num(mape * mask)
+            mape = np.mean(mape)
+            r2_new = r2(label, pred)
+        return mae, rmse, mape, r2_new
+
+
+def visualize_result(h5_file, nodes_id, time_se, visualize_file):
+    file_obj = h5py.File(h5_file, "r") # 获得文件对象，这个文件对象有两个keys："predict"和"target"
+    prediction = file_obj["predict"][:][:, :, 0]  # [N, T],切片，最后一维取第0列，所以变成二维了，要是[:, :, :1]那么维度不会缩减
+    target = file_obj["target"][:][:, :, 0]  # [N, T],同上
+    file_obj.close()
+
+    plot_prediction = prediction[nodes_id][time_se[0]: time_se[1]]  # [T1]，将指定节点的，指定时间的数据拿出来
+    plot_target = target[nodes_id][time_se[0]: time_se[1]]  # [T1]，同上
+
+    workbook = xlwt.Workbook(encoding='utf-8')
+    worksheet = workbook.add_sheet('My Worksheet')
+    worksheet.write(0, 0, "target")
+    worksheet.write(0, 1, "prediction")
+    for i in range(plot_target.shape[0]):
+        worksheet.write(i+1, 0, label=plot_target[i])
+        worksheet.write(i+1, 1, label=plot_prediction[i])
+
+    # 保存
+    # workbook.save('PeMS04_single_head.xls')
+    workbook.save('STCGAT_no_Tcn_single_lstm_y_Ls.xls')
+
+    plt.figure(figsize=(15,5))
+    plt.grid(True, linestyle="-.", linewidth=0.5)
+    plt.plot(np.array([t for t in range(time_se[1] - time_se[0])]), plot_prediction, ls="-", marker=" ", color="r")
+    plt.plot(np.array([t for t in range(time_se[1] - time_se[0])]), plot_target, ls="-", marker=" ", color="b")
+
+    plt.legend(["prediction", "target"], loc="upper right", fontsize=9)
+
+    plt.xlabel("Time", fontsize=15)
+    plt.ylabel("Traffic speed", fontsize=15)
+
+    plt.axis([0, time_se[1] - time_se[0],
+              np.min(np.array([np.min(plot_prediction), np.min(plot_target)])),
+              np.max(np.array([np.max(plot_prediction), np.max(plot_target)]))])
+
+    plt.savefig(visualize_file + ".svg")
+
